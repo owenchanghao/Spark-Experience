1057682cee86e248d8826b5185d702539ef870a3
Owen Changhao Li <50399759+owenchanghao@users.noreply.github.com>
2023-03-07 23:18:04 +0800

    On persist

--- persist.py
+++ persist.py
@@ -1,6 +1,8 @@
 ### DESCRIPTION:
+'''
 The intent of the code is to get all numbers below 50 from A and put them into B, and then get all numbers above 10 from B and put them into C.
 Add one line of code so that it produces the desired behavior. You are not allowed to change the existing code.
+'''
 
 from pyspark.sql import SparkSession
 from pyspark import SparkContext
